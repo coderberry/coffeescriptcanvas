57090bfe2c4875708e9fbb5f61e9e830850b84d7
Eric Berry <cavneb@gmail.com>
2011-10-22 15:59:38 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeescriptcanvasCom::Application.config.secret_token = 'd72c78bbd5262b421c37cc153f9d02962866a9b44799fa5e46cb353742a45fe4fb1507d202cf9e7a78ca212a9a6c90467512877d47beeb0bc7c59632f6bb6c85'
